d566a0bb099be70fc5b896f6a2a8a741fef66180
KirillKomarov550503 <kirya.komarov.97@list.ru>
2018-03-24 20:13:30 +0300

    removind DAO

--- dao/src/main/java/dev3/bank/dao/ClientDAO.java
+++
@@ -1,9 +0,0 @@
-package dev3.bank.dao;
-
-import dev3.bank.entity.Client;
-
-import java.util.Collection;
-
-public interface ClientDAO extends CrudDAO<Client> {
-    Collection<Client> getByAccountId(long accountId);
-}

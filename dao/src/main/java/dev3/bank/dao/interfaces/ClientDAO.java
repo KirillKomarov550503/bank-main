846a0a05c5f96070eb6b3a3920a530c375ae0f8e
KirillKomarov550503 <kirya.komarov.97@list.ru>
2018-03-26 20:29:11 +0300

    modify ClientDAO

--- dao/src/main/java/dev3/bank/dao/interfaces/ClientDAO.java
+++ dao/src/main/java/dev3/bank/dao/interfaces/ClientDAO.java
@@ -5,5 +5,6 @@ import dev3.bank.entity.Client;
 import java.util.Collection;
 
 public interface ClientDAO extends CrudDAO<Client> {
-    Collection<Client> getByAccountId(long accountId);
+    Client getByAccountId(long accountId);
+
 }

21d4bff157e58141f8041041649593df6db328b4
KirillKomarov550503 <kirya.komarov.97@list.ru>
2018-04-09 21:11:26 +0300

    realize ClientNewsDAOImpl

--- dao/src/main/java/dev3/bank/dao/impl/ClientNewsDAOImpl.java
+++ dao/src/main/java/dev3/bank/dao/impl/ClientNewsDAOImpl.java
@@ -3,22 +3,120 @@ package dev3.bank.dao.impl;
 import dev3.bank.dao.interfaces.ClientNewsDAO;
 import dev3.bank.entity.ClientNews;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.stream.Collectors;
 
-public class ClientNewsDAOImpl extends CrudDAOImpl<ClientNews> implements ClientNewsDAO {
-    public ClientNewsDAOImpl() {
-        super(ClientNews.class);
+public class ClientNewsDAOImpl implements ClientNewsDAO {
+    private static ClientNewsDAOImpl clientNewsDAO;
+    private Connection connection;
+
+    private ClientNewsDAOImpl(Connection connection) {
+        this.connection = connection;
+    }
+
+    public static synchronized ClientNewsDAOImpl getClientNewsDAO(Connection connection) {
+        if (clientNewsDAO == null) {
+            clientNewsDAO = new ClientNewsDAOImpl(connection);
+        }
+        return clientNewsDAO;
     }
 
     @Override
     public Collection<ClientNews> getAllByClientId(long clientId) {
-        return getEntityMapValues()
-                .stream()
-                .filter(clientNews -> (clientNews.getClient().getId() == clientId)
-                        || (clientNews.getClient().getId() == 0))
-                .collect(Collectors.toList());
+        Collection<ClientNews> clientNewsCollection = new ArrayList<>();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("" +
+                    "SELECT * FROM ClientNews WHERE client_id=? or client_id=0");
+            preparedStatement.setLong(1, clientId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                ClientNews clientNews = new ClientNews();
+                clientNews.setId(resultSet.getLong("id"));
+                clientNews.setNewsId(resultSet.getLong("news_id"));
+                clientNews.setClientId(resultSet.getLong("client_id"));
+                clientNewsCollection.add(clientNews);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return clientNewsCollection;
     }
 
 
+    @Override
+    public ClientNews getById(long id) {
+        ClientNews clientNews = new ClientNews();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("" +
+                    "SELECT * FROM ClientNews WHERE id=?");
+            preparedStatement.setLong(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                clientNews.setId(resultSet.getLong("id"));
+                clientNews.setNewsId(resultSet.getLong("news_id"));
+                clientNews.setClientId(resultSet.getLong("client_id"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return clientNews;
+    }
+
+    @Override
+    public ClientNews add(ClientNews entity) {
+        ClientNews clientNews = new ClientNews();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("" +
+                    "INSERT INTO ClientNews(news_id, client_id) VALUES(?, ?)");
+            preparedStatement.setLong(1, entity.getNewsId());
+            preparedStatement.setLong(2, entity.getClientId());
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                clientNews.setId(resultSet.getLong("id"));
+                clientNews.setNewsId(resultSet.getLong("news_id"));
+                clientNews.setClientId(resultSet.getLong("client_id"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return clientNews;
+    }
+
+    @Override
+    public ClientNews update(ClientNews entity) {
+        ClientNews clientNews = new ClientNews();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("" +
+                    "UPDATE ClientNews SET news_id=?, client_id=? WHERE id=?");
+            preparedStatement.setLong(1, entity.getNewsId());
+            preparedStatement.setLong(2, entity.getClientId());
+            preparedStatement.setLong(3, entity.getId());
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                clientNews.setId(resultSet.getLong("id"));
+                clientNews.setNewsId(resultSet.getLong("news_id"));
+                clientNews.setClientId(resultSet.getLong("client_id"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return clientNews;
+
+    }
+
+    @Override
+    public void delete(long id) {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("" +
+                    "DELETE FROM ClientNews WHERE id=?");
+            preparedStatement.setLong(1, id);
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }

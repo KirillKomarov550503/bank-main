b076e15908616d16b804a06ef23d28e931701943
KirillKomarov550503 <kirya.komarov.97@list.ru>
2018-05-02 22:25:45 +0300

    modify application.class

--- controllers/src/main/java/com/netcracker/komarov/controllers/Application.java
+++ controllers/src/main/java/com/netcracker/komarov/controllers/Application.java
@@ -1,6 +1,5 @@
 package com.netcracker.komarov.controllers;
 
-import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

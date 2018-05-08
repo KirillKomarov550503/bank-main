d36f07fa9c502124e1d8e03030e1414c8adb23c0
KirillKomarov550503 <kirya.komarov.97@list.ru>
2018-05-08 16:12:59 +0300

    done TransactionServiceImplTest

--- services/src/test/java/com/netcracker/komarov/services/impl/TransactionServiceImplTest.java
+++ services/src/test/java/com/netcracker/komarov/services/impl/TransactionServiceImplTest.java
@@ -1,67 +1,82 @@
-//package com.netcracker.komarov.services.impl;
-//
-//import com.netcracker.komarov.dao.entity.Transaction;
-//import com.netcracker.komarov.dao.utils.DataBase;
-//import com.netcracker.komarov.services.dto.TransactionDTO;
-//import com.netcracker.komarov.services.exception.TransactionException;
-//import com.netcracker.komarov.services.interfaces.AccountService;
-//import com.netcracker.komarov.services.interfaces.TransactionService;
-//import org.junit.After;
-//import org.junit.Assert;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//
-//import java.text.SimpleDateFormat;
-//import java.util.ArrayList;
-//import java.util.Collection;
-//import java.util.Date;
-//
-//public class TransactionServiceImplTest extends AbstractSpringTest {
-//
-//    @Autowired
-//    private TransactionService transactionService;
-//
-//    @Autowired
-//    private AccountService accountService;
-//
-//    @Test
-//    public void showStories() {
-//        TransactionDTO transactionDTO1 = new TransactionDTO(1, 2, 13, 1);
-//        TransactionDTO transactionDTO2 = new TransactionDTO(3, 1, 24.9, 3);
-//        TransactionDTO transactionDTO3 = new TransactionDTO(2, 3, 20, 1);
-//        accountService.refill(1);
-//        accountService.refill(3);
-//        accountService.refill(2);
-//        try {
-//            transactionService.createTransaction(transactionDTO1);
-//            transactionService.createTransaction(transactionDTO2);
-//            transactionService.createTransaction(transactionDTO3);
-//        } catch (TransactionException e) {
-//            System.out.println("TransactionException");
-//        }
-//        Collection<Transaction> transactions = new ArrayList<>();
-//        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm");
-//        String dateFormat = simpleDateFormat.format(new Date());
-//        Transaction transaction1 = new Transaction(1, dateFormat, 1, 2, 13);
-//        Transaction transaction3 = new Transaction(3, dateFormat, 2, 3, 20);
-//        transactions.add(transaction1);
-//        transactions.add(transaction3);
-//        Assert.assertEquals(transactions, transactionService.showStories(1));
-//    }
-//
-//    @Test
-//    public void createTransaction() throws TransactionException {
-//        TransactionDTO transactionDTO = new TransactionDTO();
-//        accountService.refill(3);
-//        transactionDTO.setAccountFromId(1);
-//        transactionDTO.setAccountToId(2);
-//        transactionDTO.setClientId(3);
-//        transactionDTO.setMoney(23.5);
-//        try {
-//            Assert.assertNull(transactionService.createTransaction(transactionDTO));
-//        } catch (TransactionException e) {
-//            System.out.println("Transaction exception");
-//        }
-//    }
-//}
+package com.netcracker.komarov.services.impl;
+
+import com.netcracker.komarov.services.ServiceContext;
+import com.netcracker.komarov.services.dto.entity.AccountDTO;
+import com.netcracker.komarov.services.dto.entity.PersonDTO;
+import com.netcracker.komarov.services.dto.entity.TransactionDTO;
+import com.netcracker.komarov.services.exception.TransactionException;
+import com.netcracker.komarov.services.interfaces.AccountService;
+import com.netcracker.komarov.services.interfaces.ClientService;
+import com.netcracker.komarov.services.interfaces.TransactionService;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+
+@Transactional
+@Rollback
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ServiceContext.class)
+public class TransactionServiceImplTest {
+
+    @Autowired
+    private TransactionService transactionService;
+
+    @Autowired
+    private AccountService accountService;
+
+    @Autowired
+    private ClientService clientService;
+
+    @Before
+    public void init() {
+        clientService.save(new PersonDTO(0, "Kirill", "Komarov", 1, 1));
+        clientService.save(new PersonDTO(0, "Vlad", "M", 2, 2));
+        accountService.createAccount(new AccountDTO(false, 0.0), 1);
+        accountService.createAccount(new AccountDTO(false, 0.0), 2);
+        accountService.createAccount(new AccountDTO(false, 0.0), 1);
+        accountService.refill(1);
+    }
+
+    @Test
+    public void showStories() {
+        accountService.refill(2);
+        accountService.refill(3);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm");
+        String date = simpleDateFormat.format(new Date());
+        TransactionDTO transactionDTO1 = new TransactionDTO(0, 2, 1, 13, date);
+        TransactionDTO transactionDTO2 = new TransactionDTO(0, 1, 3, 24.9, date);
+        TransactionDTO transactionDTO3 = new TransactionDTO(0, 3, 2, 60, date);
+        try {
+            transactionService.createTransaction(transactionDTO1, 2);
+            transactionService.createTransaction(transactionDTO2, 1);
+            transactionService.createTransaction(transactionDTO3, 1);
+        } catch (TransactionException e) {
+            System.out.println("TransactionException");
+        }
+        Collection<TransactionDTO> transactions = new ArrayList<>();
+        TransactionDTO transaction1 = new TransactionDTO(2, 1, 3, 24.9, date);
+        TransactionDTO transaction3 = new TransactionDTO(3, 3, 2, 60, date);
+        transactions.add(transaction1);
+        transactions.add(transaction3);
+        Assert.assertEquals(transactions, transactionService.showStories(1));
+    }
+
+    @Test(expected = TransactionException.class)
+    public void createTransaction() throws TransactionException {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm");
+        String date = simpleDateFormat.format(new Date());
+        TransactionDTO transactionDTO = new TransactionDTO(0, 1, 2, 110, date);
+        Assert.assertNull(transactionService.createTransaction(transactionDTO, 1));
+    }
+}

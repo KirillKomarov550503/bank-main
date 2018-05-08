eeea6329bb50f653bbcfb099793df8390a857866
KirillKomarov550503 <kirya.komarov.97@list.ru>
2018-05-08 13:51:16 +0300

    done PersonServiceImplTest

--- services/src/test/java/com/netcracker/komarov/services/impl/PersonServiceImplTest.java
+++ services/src/test/java/com/netcracker/komarov/services/impl/PersonServiceImplTest.java
@@ -1,38 +1,62 @@
 package com.netcracker.komarov.services.impl;
 
-import com.netcracker.komarov.dao.entity.Person;
-import com.netcracker.komarov.dao.entity.Role;
-import com.netcracker.komarov.dao.utils.DataBase;
+import com.netcracker.komarov.services.ServiceContext;
+import com.netcracker.komarov.services.dto.entity.PersonDTO;
+import com.netcracker.komarov.services.interfaces.AdminService;
+import com.netcracker.komarov.services.interfaces.ClientService;
 import com.netcracker.komarov.services.interfaces.PersonService;
-import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.Collection;
 
-public class PersonServiceImplTest extends AbstractSpringTest {
+@Transactional
+@Rollback
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ServiceContext.class)
+public class PersonServiceImplTest {
+
+    @Autowired
+    private AdminService adminService;
+
+    @Autowired
+    private ClientService clientService;
 
     @Autowired
     private PersonService personService;
 
+    @Before
+    public void init() {
+        PersonDTO personDTO1 = new PersonDTO(0, "Tony", "Stark", 1, 1);
+        PersonDTO personDTO2 = new PersonDTO(0, "Steve", "Rodgers", 2, 2);
+        PersonDTO personDTO3 = new PersonDTO(0, "Stephen", "Strange", 3, 3);
+        PersonDTO personDTO4 = new PersonDTO(0, "Peter", "Parker", 4, 4);
+        clientService.save(personDTO2);
+        adminService.addAdmin(personDTO1);
+        adminService.addAdmin(personDTO4);
+        clientService.save(personDTO3);
+    }
+
+
     @Test
     public void getAllPeople() {
-        Person person1 = new Person(1, "Kirill", "Komarov", 123, Role.CLIENT,
-                null, null, 4234);
-        Person person2 = new Person(2, "Vladislav", "Maznya", 54, Role.CLIENT,
-                null, null, 234);
-        Person person3 = new Person(3, "Pavel", "Zaretskya", 1252, Role.CLIENT,
-                null, null, 643);
-        Person person4 = new Person(4, "Vladimir", "Putin", 1111111, Role.ADMIN,
-                null, null, 1);
-        Collection<Person> people = new ArrayList<>();
-        people.add(person1);
-        people.add(person2);
-        people.add(person3);
-        people.add(person4);
-        Assert.assertEquals(people, personService.getAllPeople());
+        PersonDTO personDTO1 = new PersonDTO(2, "Tony", "Stark", 1, 1);
+        PersonDTO personDTO2 = new PersonDTO(1, "Steve", "Rodgers", 2, 2);
+        PersonDTO personDTO3 = new PersonDTO(4, "Stephen", "Strange", 3, 3);
+        PersonDTO personDTO4 = new PersonDTO(3, "Peter", "Parker", 4, 4);
+        Collection<PersonDTO> dtos = new ArrayList<>();
+        dtos.add(personDTO2);
+        dtos.add(personDTO1);
+        dtos.add(personDTO4);
+        dtos.add(personDTO3);
+        Assert.assertEquals(dtos, personService.getAllPeople());
     }
 }
